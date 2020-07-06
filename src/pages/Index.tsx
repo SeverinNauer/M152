5db8e9e77f8f97bd926d32ef8544d6c2207674c8
Severin Nauer <severin.nauer@gmail.com>
2020-07-06 21:51:06 +0200

    Add Content to Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,39 @@
 import React from "react";
-import { makeStyles, createStyles } from "@material-ui/core";
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {},
-  })
-);
+import { makeStyles, createStyles, Typography, Link } from "@material-ui/core";
+import ContentBox from "../components/ContentBox";
+
 const Index = () => {
-  const classes = useStyles();
-  return <div className={classes.root}>Welcome</div>;
+  return (
+    <ContentBox>
+      <Typography variant="h4">Modul 152 Showcase</Typography>
+      <br />
+      <Typography variant="body1">
+        Willkommen auf meiner Showcase Seite zum Modul 152. Auf dieser Website
+        wurde das{" "}
+        <Link href="https://docs.google.com/spreadsheets/d/1ThwJMPpVxhdH-PKnO9mBeibByK5aaCrvHyc6hRdd-0U/edit#gid=1190714047">
+          Kompetenzraster
+        </Link>{" "}
+        abgearbeitet.
+      </Typography>
+      <br />
+      <Typography variant="h5">Navigation</Typography>
+      <Typography variant="body1">
+        Auf der Linken seite sind sind die Punkte des Kompetenzrasters
+        aufgereit. Diese verweisen auf die Seiten zu den jeweiligen Punkte.
+        <br /> Die Seiten beinhalten Beschreibungen oder Beispiele zu den
+        jeweiligen Themen.
+      </Typography>
+      <br />
+      <Typography variant="h5">Source Code</Typography>
+      <Typography variant="body1">
+        Der Source Code zu dieser Seite kann in meinem{" "}
+        <Link href="https://github.com/SeverinNauer/M152">
+          Github Repository
+        </Link>{" "}
+        angeschaut und heruntergeladen werden.
+      </Typography>
+    </ContentBox>
+  );
 };
 
 export default Index;
