b12d06d5a055c84114820783f44b5e702e38ef73
Nauer Severin <severin.nauer@iseag.ch>
2020-07-07 10:21:01 +0200

    Add Codeexample to carousel

--- src/pages/automate/CarouselPage.tsx
+++ src/pages/automate/CarouselPage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ContentBox from "../../components/ContentBox";
-import { Typography, Card } from "@material-ui/core";
+import { Typography, Card, Link } from "@material-ui/core";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import CodeExample from "../../components/CodeExample";
 //https://pixabay.com/de/photos/landschaft-berge-panorama-4587079/
 //https://pixabay.com/de/photos/hund-tier-h%C3%BCndchen-niedlich-jung-5357794/
 //https://pixabay.com/de/photos/b%C3%A4ume-wald-sommer-griechenland-5350721/
@@ -41,8 +42,12 @@ const CarouselPage = () => {
     <ContentBox>
       <Typography variant="h4">Automatisierung</Typography>
       <br />
-      <Typography variant="h5">
-        Beispiel eines automatischen Slideshow
+      <Typography variant="h5">Automatische Slideshow</Typography>
+      <Typography variant="subtitle1">
+        Das untenstehende Beispiel benutzt das NPM Package{" "}
+        <Link href="https://www.npmjs.com/package/react-responsive-carousel">
+          react-response-carousel
+        </Link>
       </Typography>
       <Card>
         <Carousel
@@ -57,6 +62,55 @@ const CarouselPage = () => {
           ))}
         </Carousel>
       </Card>
+      <CodeExample>{`import React from "react";
+import { Card, Link } from "@material-ui/core";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+      
+interface IImage {
+  name: string;
+  alt: string;
+}
+  
+const Image = (props: IImage) => {
+  const { name, alt } = props;
+  return (
+    <div>
+      <img
+        src={\`\${process.env.PUBLIC_URL}/assets/carousel/\${name}\`}
+        alt={alt}
+      />
+    </div>
+  );
+};
+
+const Images: IImage[] = [
+  { name: "birds-flight.jpg", alt: "birds-flight" },
+  { name: "cornflowers.jpg", alt: "cornflowers" },
+  { name: "dogo.jpg", alt: "dogo" },
+  { name: "landscape.jpg", alt: "landscape" },
+  { name: "river.jpg", alt: "river" },
+  { name: "trees.jpg", alt: "trees" },
+];
+
+const CarouselPage = () => {
+  return (
+    <Card>
+      <Carousel
+          autoPlay
+          swipeable
+          infiniteLoop
+          interval={2000}
+          showThumbs={false}
+      >
+        {Images.map((image, index) => (
+        <Image key={index} {...image} />
+        ))}
+      </Carousel>
+    </Card>
+  );
+};
+      `}</CodeExample>
     </ContentBox>
   );
 };
